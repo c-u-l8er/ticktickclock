29ea5ec1ef5985e39413b59a2b209b795744a9a5
Travis Burandt <travis.burandt@gmail.com>
2025-03-05 00:46:24 -0600

    tick tick clock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Alert } from "flowbite-svelte";
+</script>
+
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">TICK TICK</span>
+        CLOCK
+    </Alert>
+</div>
